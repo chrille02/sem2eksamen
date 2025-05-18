a6128ad951827f3b9f4ff249f5128a56bd1d1786
chrill <97027857+chrille02@users.noreply.github.com>
2025-05-18 17:53:20 +0200

    Add files via upload

---
+++ image comparison slider/BeforeAfter.js
@@ -0,0 +1,20 @@
+// Vælg alle .image-comparison sektioner
+document.querySelectorAll(".image-comparison").forEach((comparison) => {
+  // Find elementerne vi skal manipulere
+  const slider = comparison.querySelector(".slider");
+  const beforeImage = comparison.querySelector(".before-image");
+  const sliderLine = comparison.querySelector(".slider-line");
+  const sliderIcon = comparison.querySelector(".slider-icon");
+
+  // Når brugeren ændrer sliderens værdi
+  slider.addEventListener("input", (e) => {
+    let sliderValue = e.target.value + "%";
+
+    // Opdater før-billedets bredde
+    beforeImage.style.width = sliderValue;
+    // Flyt slider-linjen
+    sliderLine.style.left = sliderValue;
+    // Flyt ikonet
+    sliderIcon.style.left = sliderValue;
+  });
+});
